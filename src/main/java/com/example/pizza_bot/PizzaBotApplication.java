7a438ec9d40ba813c744a99a88d7838eeca25f12
Anthony Bastard <abastard05@orange.fr>
2023-02-24 09:50:39 +0100

    Initial start Commit

---
+++ src/main/java/com/example/pizza_bot/PizzaBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.pizza_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pizza_bot/PizzaBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pizza_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
