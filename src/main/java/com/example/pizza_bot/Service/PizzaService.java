b09d69fc426ebe6071d4e01646158dfcb72d5f25
AnthonyB05 <77343444+AnthonyB05@users.noreply.github.com>
2023-02-24 10:31:42 +0100

    Try bug fix

--- src/main/java/com/example/pizza_bot/Service/PizzaService.java
+++ src/main/java/com/example/pizza_bot/Service/PizzaService.java
@@ -17,17 +17,79 @@ public class PizzaService {
 
     private final List<String> genres = List.of("Tomate", "Crème", "Sucre");
 
+    private final List<List<String>> tomate = List.of( List.of("MARGUERITE","16,00€"),  List.of("ROYALE","16,00€"),  List.of("BOLOGNAISE","16,00€"));
+    private final List<String> creme = List.of("SAUMON", "MONTAGNARDE", "HAWAIENNE");
+    private final List<String> sucre = List.of("NUTELLA", "FRAISE", "CHOCOLAT");
+
     public WebhookReponseDTO getPizza(QueryRequestDTO queryRequestDTO) {
+
+       /* if (queryRequestDTO.getQueryResult().getParameters().getBase() == null) {
+            return getbase(false);
+        } else {
+            return getPizzaByBase(queryRequestDTO.getQueryResult().getParameters().getBase().toLowerCase());
+        }*/
+
         WebhookReponseDTO webhookReponseDTO = new WebhookReponseDTO();
         return webhookReponseDTO;
     }
+
+    /*private WebhookReponseDTO getPizzaByBase(String toLowerCase) {
+        WebhookReponseDTO webhookReponseDTO = new WebhookReponseDTO();
+
+        List<MessageDTO> messageDTOList = new ArrayList<>();
+        MessageDTO messageSimpleResponse = new MessageDTO();
+        messageSimpleResponse.setPlatform("ACTIONS_ON_GOOGLE");
+        SimpleResponsesDTO simpleResponsesDTO = new SimpleResponsesDTO();
+        SimpleResponseDTO simpleResponseDTO = new SimpleResponseDTO();
+        simpleResponseDTO.setTextToSpeech("Voici les pizzas que je te propose !!");
+        simpleResponsesDTO.setSimpleResponses(List.of(simpleResponseDTO));
+        messageSimpleResponse.setSimpleResponses(simpleResponsesDTO);
+        messageDTOList.add(messageSimpleResponse);
+
+        if (toLowerCase.equals("tomate")) {
+            for (String pizza : tomate) {
+                MessageDTO messageSuggestion = new MessageDTO();
+                messageSuggestion.setPlatform("ACTIONS_ON_GOOGLE");
+                SuggestionsDTO suggestionsDTO = new SuggestionsDTO();
+                SuggestionDTO suggestionDTO = new SuggestionDTO();
+                suggestionDTO.setTitle(pizza);
+                suggestionsDTO.setSuggestions(List.of(suggestionDTO));
+                messageSuggestion.setSuggestions(suggestionsDTO);
+                messageDTOList.add(messageSuggestion);
+            }
+        } else if (toLowerCase.equals("crème")) {
+            for (String pizza : creme) {
+                MessageDTO messageSuggestion = new MessageDTO();
+                messageSuggestion.setPlatform("ACTIONS_ON_GOOGLE");
+                SuggestionsDTO suggestionsDTO = new SuggestionsDTO();
+                SuggestionDTO suggestionDTO = new SuggestionDTO();
+                suggestionDTO.setTitle(pizza);
+                suggestionsDTO.setSuggestions(List.of(suggestionDTO));
+                messageSuggestion.setSuggestions(suggestionsDTO);
+                messageDTOList.add(messageSuggestion);
+            }
+        } else if (toLowerCase.equals("sucre")) {
+            for (String pizza : sucre) {
+                MessageDTO messageSuggestion = new MessageDTO();
+                messageSuggestion.setPlatform("ACTIONS_ON_GOOGLE");
+                SuggestionsDTO suggestionsDTO = new SuggestionsDTO();
+                SuggestionDTO suggestionDTO = new SuggestionDTO();
+                suggestionDTO.setTitle(pizza);
+                suggestionsDTO.setSuggestions(List.of(suggestionDTO));
+                messageSuggestion.setSuggestions(suggestionsDTO);
+                messageDTOList.add(messageSuggestion);
+            }
+        }
+
+
+    }*/
 
     public WebhookReponseDTO getbase(Boolean error) {
 
         WebhookReponseDTO webhookReponseDTO = new WebhookReponseDTO();
         List<MessageDTO> messageDTOList = new ArrayList<>();
 
-        if(error) {
+        if (error) {
             MessageDTO messageSimpleResponse = new MessageDTO();
             messageSimpleResponse.setPlatform("ACTIONS_ON_GOOGLE");
             SimpleResponsesDTO simpleResponsesDTO = new SimpleResponsesDTO();
